5b3bbe5e6621e9b3f5435dbf4d2d058f5d3aba00
evertonbravo <everton.bravo@gmail.com>
2020-04-09 11:08:09 -0300

    Arquivos restantes
    Comit

---
+++ GestaoDeCondominioTestes/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GestaoDeCondominioTestes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
